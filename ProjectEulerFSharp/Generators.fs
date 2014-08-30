53fba39bd7ce14c1cf3890106d014bcad591381f
Nathen Drees <nadrees@gmail.com>
2014-08-30 13:51:21 -0500

    Finished problems 1 and 2

---
+++ ProjectEulerFSharp/Generators.fs
@@ -0,0 +1,13 @@
+﻿module Generators
+
+// define the fibonnaci generator
+// this uses an internal recursive function to generate an infinite list
+// of fibonacci numbers, starting with 1, 1
+let fibonnaci = 
+    // define a recursive function which computes fibonnaci numbers
+    let rec fibgenerator n1 n2 =
+        seq { let next = n1 + n2
+              yield next
+              yield! fibgenerator next n1 }
+    // start a sequence with 1,1, then generate the rest
+    seq { yield 1; yield 1; yield! fibgenerator 1 1 }
